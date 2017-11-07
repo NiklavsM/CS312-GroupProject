e39974348eb6b3988871008789c6389b4c205cb3
Jordan Scott Howorth CS2015 <jordan.howorth.2015@uni.strath.ac.uk>
2017-11-07 21:35:44 +0000

    Test push

---
+++ display/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once "header.php";
+?>
+<!--Actual page content goes here-->
+     <div class="jumbotron container">
+        <div class="row">
+            <p><?php if ($successfulLogin) {
+                    echo "Welcome back, $username. Have a free car.";
+                } else {
+                    echo "You should probably login.";
+                } ?></p>
+        </div>
+    </div>
+<!--Optional Javascript goes here-->
+<?php
+include_once "footer.php";
+?>
