bd7aca8fa63aa5818cc5e5e24e3b943b0ad560b0
skb15165 <skb15165@uni.strath.ac.uk>
2017-11-15 12:50:25 +0000

    Updated navbar

--- display/dependencies/header.php
+++ display/dependencies/header.php
@@ -1,69 +1,69 @@
 <?php
-session_start();
-include_once "~/../../model/database.php";
-$successfulLogin = False;
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $password = "";
-    $username = "";
-    $confirmPassword = "";
-
-    //If user wants to register
-    //confirmPassword will only be set if the user tried to register
-    if (isset($_POST['confirmPassword'])) {
-        //Do registration
-        $password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
-        $confirmPassword = isset($_POST['confirmPassword']) ? filter_var($_POST['confirmPassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
-        $username = isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
-        $successfulRegistration = $confirmPassword == $password;
-
-        if ($password != "" && $username != "" && $successfulRegistration) {
-            $sql = "Insert into `User` (`username`, `password`) VALUES (?, ?)";
-            if ($stmt = $conn->prepare($sql)) {
-                $password = md5($password);
-                $stmt->bind_param("ss", $username, $password);
-                $successfulRegistration = $stmt->execute();
-            } else {
-
-            }
-        }
-        if ($successfulRegistration) {
-            $_SESSION["username"] = $username;
-            $_SESSION["password"] = $password;
-        }
-    }
-
-    //Otherwise for login...
-    //if it wasn't set, let's attempt to set login info. no effect if no user/pass
-    else {
-        //Do login
-        $password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
-        $username = isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
-        $successfulLogin = $password != "" && $username != "";
-
-        if ($successfulLogin) {
-            $_SESSION['password'] = md5($password);
-            $_SESSION['username'] = $username;
-        }
-    }
-}
-
-//Check if login details are available
-if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && isset($_SESSION['username'])) {
-    //Do login
-    $successfulLogin = False;
-    $sql = "SELECT * FROM `User` WHERE `username`= ? and `password` = ?";
-    $password = filter_var($_SESSION['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $username = filter_var($_SESSION['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-    if ($stmt = $conn->prepare($sql)) {
-        if ($stmt->bind_param("ss", $username, $password)) {
-            if ($successfulLogin = $stmt->execute()) {
-                $successfulLogin = $stmt->get_result()->num_rows > 0;
-            }
-        }
-    }
-} ?>
+//session_start();
+//include_once "~../../../Model/database.php";
+//$successfulLogin = False;
+//
+//if ($_SERVER['REQUEST_METHOD'] === "POST") {
+//    $password = "";
+//    $username = "";
+//    $confirmPassword = "";
+//
+//    //If user wants to register
+//    //confirmPassword will only be set if the user tried to register
+//    if (isset($_POST['confirmPassword'])) {
+//        //Do registration
+//        $password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
+//        $confirmPassword = isset($_POST['confirmPassword']) ? filter_var($_POST['confirmPassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
+//        $username = isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
+//        $successfulRegistration = $confirmPassword == $password;
+//
+//        if ($password != "" && $username != "" && $successfulRegistration) {
+//            $sql = "Insert into `User` (`username`, `password`) VALUES (?, ?)";
+//            if ($stmt = $conn->prepare($sql)) {
+//                $password = md5($password);
+//                $stmt->bind_param("ss", $username, $password);
+//                $successfulRegistration = $stmt->execute();
+//            } else {
+//
+//            }
+//        }
+//        if ($successfulRegistration) {
+//            $_SESSION["username"] = $username;
+//            $_SESSION["password"] = $password;
+//        }
+//    }
+//
+//    //Otherwise for login...
+//    //if it wasn't set, let's attempt to set login info. no effect if no user/pass
+//    else {
+//        //Do login
+//        $password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
+//        $username = isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "";
+//        $successfulLogin = $password != "" && $username != "";
+//
+//        if ($successfulLogin) {
+//            $_SESSION['password'] = md5($password);
+//            $_SESSION['username'] = $username;
+//        }
+//    }
+//}
+//
+////Check if login details are available
+//if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && isset($_SESSION['username'])) {
+//    //Do login
+//    $successfulLogin = False;
+//    $sql = "SELECT * FROM `User` WHERE `username`= ? and `password` = ?";
+//    $password = filter_var($_SESSION['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+//    $username = filter_var($_SESSION['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+//
+//    if ($stmt = $conn->prepare($sql)) {
+//        if ($stmt->bind_param("ss", $username, $password)) {
+//            if ($successfulLogin = $stmt->execute()) {
+//                $successfulLogin = $stmt->get_result()->num_rows > 0;
+//            }
+//        }
+//    }
+//} ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -72,7 +72,7 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && is
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
           integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <!--Load our own style sheet for this page-->
-    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="~../../../css/index.css">
     <!--A viewport specifies how much of the page can be seen. Allows easy resizing-->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -82,28 +82,33 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && is
 <body>
 
 <!-- Navigation menu !-->
+<nav class="navbar navar-expand-md navbar-dark bg-dark item">
+    <div>
+        <ul class="nav menu_list" role="tablist" >
+            <li class="menu_item">
+                <a  class="" href="index.php" >Home</a>
+            </li>
+            <li class=" menu_item">
+                <a data-target="#register_box" class="" role="tab" data-toggle="modal">Cars</a>
+            </li>
+            <li class=" menu_item">
+                <a data-target="#login_box" class="" role="tab" data-toggle="modal">Something else</a>
+            </li>
+        </ul>
+    </div>
 
-<div class="row" , id="main_text">
-    <button type="button" class="btn btn-info btn-lg menu_item" data-toggle="modal" data-target="#register_box">
-        Register
-    </button>
-    <button type="button" class="btn btn-info btn-lg menu_item" data-toggle="modal" data-target="#login_box">Login
-    </button>
-</div>
-<ul class="nav nav-tabs" role="tablist">
-    <li class="nav-item">
-        <a href="index.php" class="nav-link active" role="tab" data-toggle="tab">Home</a>
-    </li>
-    <li class="nav-item">
-        <a data-target="#register_box" class="nav-link" role="tab" data-toggle="modal">Cars</a>
-    </li>
-    <li class="nav-item">
-        <a data-target="#login_box" class="nav-link" role="tab" data-toggle="modal">Something else</a>
-    </li>
-</ul>
+    <div>
+        <ul class="nav menu_list " role="tablist">
+            <li class="nav-item menu_item menu_item2">
+                <a data-target="#register_box" class="" role="tab" data-toggle="modal">Register</a>
+            </li>
+            <li class="nav-item menu_item menu_item2">
+                <a data-target="#login_box" class="" role="tab" data-toggle="modal">Login</a>
+            </li>
+        </ul>
+    </div>
 
-
-
+</nav>
 
 <!--Title-->
 <div class="jumbotron text-center" id="title">
@@ -112,9 +117,10 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && is
 </div>
 
 <?php
-    include_once "modals/registration_modal.php";
+include_once "modals/registration_modal.php";
 ?>
 
 <?php
-    include_once "modals/login_modal.php";
+include_once "modals/login_modal.php";
 ?>
+
