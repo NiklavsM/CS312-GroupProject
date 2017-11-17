69dff62e933182ffc1c15ea48db0e4fc2410bfa1
Niklavs Meiers <niklavs@burti.lv>
2017-11-17 18:09:42 +0000

    sorting out imports

--- display/dependencies/header.php
+++ display/dependencies/header.php
@@ -68,7 +68,6 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['password']) && is
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!--Load bootstrap's style sheet. It includes an opinionated reboot.-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
           integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
